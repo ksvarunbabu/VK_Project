b46be18fc7f1f64a7b8e7b4a4c30170693d0b6a2
Varun K S <ksvarun@gmail.com>
2023-08-06 01:13:53 -0500

    Tea_site

--- VK_Website/mywebsite/myapp/views.py
+++ VK_Website/mywebsite/myapp/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 # from django.http import HttpResponse
 
-
+teas = [
+    {'id': 1, 'name': 'Black Tea'},
+    {'id': 2, 'name': 'Indian Masala Tea'},
+    {'id': 3, 'name': 'Green Tea'},
+]
 
 #Creating a default home request directing to a Welcome Message
 def home(request):
     # return HttpResponse("Welcome to VK HomePage")
-    return render(request, 'home.html') # render function is used to point the html files inside template folder
+    # return render(request, 'myapp/home.html', {'teas': teas}) # render function is used to point the html files inside template folder
+    context = {'teas' : teas}
+    return render(request, 'myapp/home.html', context)
 def enter(request):
     # return HttpResponse("Entering VK Site")
-    return render(request, 'enter.html')
+    return render(request, 'myapp/enter.html')
